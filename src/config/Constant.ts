3328b92ec8f69299485101e8c9d08d7165ebda5e
Silvio Hinkelman <silviohink@gmail.com>
2021-12-28 15:10:01 -0300

    Update Constant.ts

--- src/config/Constant.ts
+++ src/config/Constant.ts
@@ -1,5 +1,5 @@
 export const config = {
   API_URL: 'http://localhost:5000',
   MONGO_CONNECTION:
-  'mongodb+srv://dio:dio@encurtadorurl.eakbt.mongodb.net/encurtadorURL?retryWrites=true&w=majority',
+  'mongodb+srv:// <usuário>: <senha>@encurtadorurl.eakbt.mongodb.net/encurtadorURL?retryWrites=true&w=majority',
 }
