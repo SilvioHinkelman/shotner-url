9ffb3d86a20420da5809bc7007104c2bd740b580
unknown <silviohink@gmail.com>
2021-12-28 14:56:26 -0300

    versão one shortner

---
+++ dist/config/Constant.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    API_URL: 'htp://localhost:5000',
+    MONGO_CONNECTION: 'mongodb+srv://dio:dio@encurtadorurl.eakbt.mongodb.net/encurtadorURL?retryWrites=true&w=majority',
+};
+//# sourceMappingURL=Constant.js.map
